c13c94042b77f497294730b41d735db0cac24a7a
Alysson Oliveira <3oalysson@gmail.com>
2023-10-30 18:21:10 -0300

    inicial

---
+++ src/main/java/edu/senac/senacwebtest/SenacWebTestApplication.java
@@ -0,0 +1,13 @@
+package edu.senac.senacwebtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenacWebTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SenacWebTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/senac/senacwebtest/SenacWebTestApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.senac.senacwebtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenacWebTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
